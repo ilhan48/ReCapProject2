00c15e158c3eea38130435b43811934dfd2f8a76
ilhan48 <58470523+ilhan48@users.noreply.github.com>
2021-02-17 03:42:22 +0300

    Updated

--- DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -19,14 +19,14 @@ namespace DataAccess.Concrete.EntityFramework
             {
                 var result = from c in context.Cars
                              join b in context.Brands
-                             on c.BrandId equals b.BrandId
+                             on c.BrandId equals b.Id
                              join co in context.Colors
-                             on c.ColorId equals co.ColorId
+                             on c.ColorId equals co.Id
                              select new CarDetailDto
                              {
                                  Id = c.Id,
-                                 BrandName = b.BrandName,
-                                 ColorName = co.ColorName,
+                                 BrandName = b.Name,
+                                 ColorName = co.Name,
                                  DailyPrice = c.DailyPrice
                              };
                 return result.ToList();
