0254c7136c8b0ddb04ca7c23848ca971044ca202
ilhan48 <58470523+ilhan48@users.noreply.github.com>
2021-02-21 23:37:02 +0300

    WebAPI is being designed

---
+++ WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Entities.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Business.Abstract;
+using Business.Concrete;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        ICarService _carService;
+
+        public CarsController(ICarService carService)
+        {
+            _carService = carService;
+        }
+
+        [HttpGet]
+        public List<Car> Get()
+        {
+            
+            var result = _carService.GetAll();
+            return result.Data;
+
+        }
+    }
+}
